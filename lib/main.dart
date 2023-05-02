75abaa98a7be99db10975d45162a70e490a527af
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-02 09:12:56 +0000

    added IndieFlower font in the body

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Material flutter package -> creates a UI according to material design guidelines specified by android
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyApp',
+void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Jiji's App"),
+          title: Text('My App'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text('Hello, world!'),
+          child: Text(
+            'Hello and welcome',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          backgroundColor: Colors.purple,
+          onPressed: () {
+            print('Dont push this button');
+          },
         ),
       ),
-    );
-  }
-}
+    ));
