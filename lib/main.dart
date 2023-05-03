7f988da068320ffac3f047bab62b219703b2dac0
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-03 06:44:41 +0000

    Home change from scaffold to Home instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart'; // Material flutter package -> creates a UI according to material design guidelines specified by android
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hello and welcome',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key}); // ? const
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello and welcome',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-          backgroundColor: Colors.purple,
-          onPressed: () {
-            print('Dont push this button');
-          },
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          print('Dont push this button');
+        },
+      ),
+    );
+  }
+}
