2042664cd8f96895c0c847fc7a7e0e18dd37ee53
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-03 06:59:05 +0000

    added network image

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,22 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: Text(
-          'Hello and welcome',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
+      body: const Center(
+          child: Image(
+        image: NetworkImage(
+            'https://images.unsplash.com/photo-1682588383984-9bce3d05c8a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=681&q=80'),
+      )
+          // child: Text(
+          //   'Hello and welcome',
+          //   style: TextStyle(
+          //     fontSize: 20.0,
+          //     fontWeight: FontWeight.bold,
+          //     letterSpacing: 2.0,
+          //     color: Colors.grey[600],
+          //     fontFamily: 'IndieFlower',
+          //   ),
+          // ),
           ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         child: Text('Click'),
         backgroundColor: Colors.purple,
